967ead84a4ebc1ed38aea1013ce2da97c2cf4cdc
jitunmohajan <jitunmohajan@gmail.com>
2019-05-03 02:09:24 +0600

    new files updated

---
+++ Data structure/segmenttree_final.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int>tree,arr;
+void init(int node,int b,int e){
+	if(b==e){
+		tree[node]=arr[b];
+		return;
+	}
+	int left=2*node;
+	int right=(2*node)+1;
+	int mid=(b+e)/2;
+	init(left,b,mid);
+	init(right,mid+1,e);
+	tree[node]=tree[left]+tree[right];
+}
+
+int query(int node,int b,int e,int i,int j){
+	if(i>e||j<b)
+		return 0;
+	if(i<=b&&j>=e)
+		return tree[node];
+	int left=2*node;
+	int right=2*node+1;
+	int mid=(b+e)/2;
+	int q1=query(left,b,mid,i,j);
+	int q2=query(right,mid+1,e,i,j);
+	return q1+q2;
+}
+void update(int node,int b,int e,int i,int newvalue){
+	if(i<b||i>e)
+		return;
+	if(b==e){
+		tree[node]=newvalue;
+		return;
+	}
+	int left=2*node;
+	int right=2*node+1;
+	int mid=(b+e)/2;
+	update(left,b,mid,i,newvalue);
+	update(right,mid+1,e,i,newvalue);
+	tree[node]=tree[left]+tree[right];
+}
+
+int main(){
+	//#ifdef ONLINE_JUDGE
+	//  	freopen("input.txt","r",stdin);
+  //		freopen("output.txt","w",stdout);
+//	#endif
+	 int n;
+    cin >> n;
+    tree.assign(n*4,0);
+    arr.assign(n+2,0);
+    for(int i=1;i<=n;i++)
+    cin>> arr[i];
+    init(1, 1, n);
+
+   // update(1, 1, n, 2, 0);
+    cout << query(1, 1, n, 1, 3) << endl;
+    update(1, 1, n, 2, 2);
+    cout << query(1, 1, n, 2, 2) << endl;
+   
+	return 0;
+
+}
---
+++ Data structure/trie_final.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node
+{
+  node* next[27]={0};
+  int ending=0;
+}*root;
+
+int insert(string s){
+
+   node* curr=root;
+   for(int i=0;i<s.length();i++)
+      {
+        int id=s[i]-'a';
+        if(curr->next[id]==NULL)
+           curr->next[id]=new node();
+      curr=curr->next[id];
+      }
+   curr->ending=1;
+}
+
+bool search(string s){
+
+  node* curr=root;
+  for(int i=0;i<s.length();i++)
+    {
+     int id=s[i]-'a';
+     if(curr->next[id]==NULL)
+        return 0;
+     curr=curr->next[id];
+    }
+ return curr->ending;
+
+}
+void del(node* curr)
+{
+    for (int i = 0; i < 26; i++)
+        if (curr->next[i])
+            del(curr->next[i]);
+
+    delete (cur);
+}
+int main(){
+
+root=new node();
+string s;
+int num_word;
+cout<<"number of word:";
+cin>>num_word;
+for(int i=0;i<num_word;i++)
+    cin>>s,insert(s);
+
+int quary;
+cout<<"number of quary:";
+cin>>quary;
+for(int i=0;i<quary;i++)
+{
+    cin>>s;
+    if(search(s))cout<<"YES"<<endl;
+    else          cout<<"NO"<<endl;
+
+}
+
+del(root);
+  return 0;
+
+}
+
