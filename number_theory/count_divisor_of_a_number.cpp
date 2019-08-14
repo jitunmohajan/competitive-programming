28476880926500069eb47707e58807f4278fa8c9
jitunmohajan <jitunmohajan@gmail.com>
2019-08-14 21:45:28 +0600

    update the count divisor code

--- number_theory/count_divisor_of_a_number.cpp
+++ number_theory/count_divisor_of_a_number.cpp
@@ -1,6 +1,17 @@
 //https://www.geeksforgeeks.org/count-divisors-n-on13/
 #include <bits/stdc++.h> 
 using namespace std; 
+/* for better solution save how many divisor of the number
+//this is according to the seive algorithm
+#define mx 100000
+vector<int>dp(mx,0);
+void divisor(){
+    for(int i=1;i<=mx;i++)
+        for(int j=i;j<=mx;j+=i)
+            dp[j]++;
+
+}
+*/
 int countDivisors(int n){ 
     int cnt = 0; 
     for (int i = 1; i <= sqrt(n); i++) { 
