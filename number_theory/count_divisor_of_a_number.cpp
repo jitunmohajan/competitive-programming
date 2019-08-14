18dcc844b585ee964ccdf9de0bd23c9da7f5ffb7
jitunmohajan <jitunmohajan@gmail.com>
2019-08-14 19:44:26 +0600

    adding new file

---
+++ number_theory/count_divisor_of_a_number.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+int countDivisors(int n){ 
+    int cnt = 0; 
+    for (int i = 1; i <= sqrt(n); i++) { 
+        if (n % i == 0) { 
+            if (n / i == i) 
+                cnt++; 
+  
+            else 
+                cnt = cnt + 2; 
+        } 
+    } 
+    return cnt; 
+} 
+int main() { 
+    cout<<countDivisors(100); 
+    return 0; 
+} 
