2d44a137f630ae10f4fa415f264e7d536eceb9d7
Jitun Mohajan <38392683+jitunmohajan@users.noreply.github.com>
2019-08-24 00:25:58 +0600

    update

---
+++ input & output file_setup.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+	freopen("error.txt","w",stderr);
+  #endif
+    return 0;
+ }
