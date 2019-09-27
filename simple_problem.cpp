584b8ec450ec3ae7216f9addfc4153e4b40ec17c
jitunmohajan <jitunmohajan@gmail.com>
2019-09-27 21:09:04 +0600

    file edit simple code

--- simple_problem.cpp
+++ simple_problem.cpp
@@ -1,13 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define io ios::base_sync_stdio(NULL);cin.tie(NULL);cout.tie(NULL);
+#define io ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+int srl=0;
 int main(){
 
-	//int a,b,c;
+	int a,b,c;
 	//cin>>a>>b>>c;
-	//cout<<"";
+	//cout<<a+b+c;
 	//printf("");
-	//printf("%d the line",a);
-	//printf("Case %d: the ans is %d",a,b);
+	//printf("%d the line",1);
+	//printf("Case %d: the ans is %d",++srl,12);
 	return 0;
 }
+
+/*
+int n;
+cin>>n;
+if(n="")
+	printf("");
+else if("")
+	printf("");
+else
+	printf("");
+
+
+*/
